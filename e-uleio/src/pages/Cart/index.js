d28ffd59ad13f756d39b91228d64fc520da6aadd
karol marques <karoldm1579@gmail.com>
2021-07-28 20:27:06 -0300

    add effect to button and span

--- e-uleio/src/pages/Cart/index.js
+++ e-uleio/src/pages/Cart/index.js
@@ -5,13 +5,18 @@ function Cart() {
     return (
         <div className='cart-container'>
             <h2>Carrinho</h2>
-            <div className='cart-info'>
+            <div className='cart-content'>
                 <div className='cart-itens'>
-
+                    <CartItem />
+                    <CartItem />
                 </div>
-                <div className='payment'>
+                <div className='payment-info'>
                     <span>Total: R${ }</span>
                     <span>Itens: { }</span>
+                </div>
+                <div className='payment-buton'>
+                    <button>Comprar</button>
+                    <span>Limpar</span>
                 </div>
             </div>
         </div>
