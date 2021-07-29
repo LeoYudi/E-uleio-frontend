b0a4399a49fd3e3de1233a438407549669f54577
karol marques <karoldm1579@gmail.com>
2021-07-28 21:36:11 -0300

    fix display cart itens

--- e-uleio/src/pages/Cart/index.js
+++ e-uleio/src/pages/Cart/index.js
@@ -11,16 +11,22 @@ function Cart() {
         <div className='cart-container'>
             <h2>Carrinho</h2>
             <div className='cart-content'>
-                <div className='cart-itens'>
+                <div className='cart-items'>
+                    <CartItem product={productTeste} />
+                    <CartItem product={productTeste} />
+                    <CartItem product={productTeste} />
+                    <CartItem product={productTeste} />
                     <CartItem product={productTeste} />
                 </div>
-                <div className='payment-info'>
-                    <span>Total: R${ }</span>
-                    <span>Itens: { }</span>
-                </div>
-                <div className='payment-buton'>
-                    <button>Comprar</button>
-                    <span>Limpar</span>
+                <div className='payment'>
+                    <div className='payment-info'>
+                        <span>Total: R${ }</span>
+                        <span>Itens: { }</span>
+                    </div>
+                    <div className='payment-button'>
+                        <button>Comprar</button>
+                        <span>Limpar</span>
+                    </div>
                 </div>
             </div>
         </div>
