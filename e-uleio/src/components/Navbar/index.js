c9b7b87326a5267d82f1dae60259582fe1e1352f
karol marques <karoldm1579@gmail.com>
2021-08-02 17:05:01 -0300

    add link to navbar

--- e-uleio/src/components/Navbar/index.js
+++ e-uleio/src/components/Navbar/index.js
@@ -6,28 +6,52 @@ import { FiBook } from "react-icons/fi";
 import { FiShoppingCart } from "react-icons/fi";
 import { FiLogIn } from "react-icons/fi";
 import { FiSearch } from "react-icons/fi";
+import { FiUser } from "react-icons/fi";
+import { isAuthenticated } from '../../services/auth';
+import { NavLink } from 'react-router-dom';
 
 
 function Navbar() {
-    return (
-        <div className="navbar-container">
-            <div className="navbar-content">
-                <div className="menu-item">
-                    <FiBook /> Livros
-                </div>
-                <div className="menu-item">
-                    <FiLogIn /> Entrar
-                </div>
-                <div className="menu-item">
-                    <FiShoppingCart /> Carrinho
-                </div>
-                <div className="menu-item">
-                    <input type="text" />
-                    <FiSearch />
+    if (isAuthenticated())
+        return (
+            <div className="navbar-container">
+                <div className="navbar-content">
+                    <NavLink to='/' className="menu-item">
+                        <FiBook /> Livros
+                    </NavLink>
+                    <NavLink to='/profile' className="menu-item">
+                        <FiUser to='/profile' /> Perfil
+                    </NavLink>
+                    <NavLink to='/cart' className="menu-item">
+                        <FiShoppingCart /> Carrinho
+                    </NavLink>
+                    <div className="menu-item">
+                        <input type="text" />
+                        <FiSearch />
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        );
+    else
+        return (
+            <div className="navbar-container">
+                <div className="navbar-content">
+                    <NavLink to='/' className="menu-item">
+                        <FiBook /> Livros
+                    </NavLink>
+                    <NavLink to='/login' className="menu-item">
+                        <FiLogIn /> Entrar
+                    </NavLink>
+                    <NavLink to='/cart' className="menu-item">
+                        <FiShoppingCart /> Carrinho
+                    </NavLink>
+                    <div className="menu-item">
+                        <input type="text" />
+                        <FiSearch />
+                    </div>
+                </div>
+            </div>
+        );
 }
 
 
