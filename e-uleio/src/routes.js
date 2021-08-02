8ec2d8a0671ed333a25faba9884b830665c94527
karol marques <karoldm1579@gmail.com>
2021-08-02 13:43:31 -0300

    create private route profile

--- e-uleio/src/routes.js
+++ e-uleio/src/routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import { isAuthenticated } from './servies/auth';
 
 import Cart from './pages/Ecommerce/Cart';
 import Home from './pages/Ecommerce/Home';
-
 import Ecommerce from './pages/Ecommerce';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 export const EcommerceRoutes = () => {
     return (
@@ -15,10 +18,25 @@ export const EcommerceRoutes = () => {
     );
 }
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            isAuthenticated() ? (
+                <Component {...props} />
+            ) : (
+                <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+            )
+        }
+    />
+);
+
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
+                <PrivateRoute path='/profile' component={Profile} />
+                <Route path="/login" component={Login} />
                 <Route path="/" component={Ecommerce} />
             </Switch>
         </BrowserRouter>
