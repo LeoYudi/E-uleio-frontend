102d74b807c58754be4793b4bd68e21edef51091
karol marques <karoldm1579@gmail.com>
2021-08-02 13:42:55 -0300

    redirecionar para perfil apos login

--- e-uleio/src/pages/Login/index.js
+++ e-uleio/src/pages/Login/index.js
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import api from '../../servies/api';
+import { login } from '../../servies/auth';
+
 import './style.css';
+import { useHistory, withRouter } from 'react-router-dom';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    let history = useHistory();
+
+    async function handleLogin(e) {
+
+        e.preventDefault();
+
+        try {
+            const response = await api.post('login', {
+                email,
+                password
+            });
+
+            if (response.status === 200) {
+                login(response.data.token);
+                history.push('/profile');
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     return (
         <div className='login-container'>
             <Header />
             <div className='login-content'>
                 <h2>Entrar</h2>
-                <form className='login-form'>
-                    <Input placeholder={'Email'} id={'email'} />
-                    <Input placeholder={'Senha'} id={'password'} />
-                    <Button title={'Entrar'} width='25rem' />
+                <form className='login-form' onSubmit={handleLogin}>
+                    <Input
+                        placeholder={'Email'}
+                        id={'email'}
+                        type='email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <Input
+                        placeholder={'Senha'}
+                        id={'password'}
+                        type='password'
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <Button title={'Entrar'} type='submit' />
                 </form>
                 <a href='/'>Ainda nao tem uma conta ? Registre-se.</a>
             </div>
@@ -23,4 +65,4 @@ function Login() {
     );
 }
 
-export default Login;
+export default withRouter(Login);
