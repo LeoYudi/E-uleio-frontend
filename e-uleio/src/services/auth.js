a8f899537b435cba674edec3e85d838825b95e6f
karol marques <karoldm1579@gmail.com>
2021-08-18 17:13:15 -0300

    add expiration time on auth

--- e-uleio/src/services/auth.js
+++ e-uleio/src/services/auth.js
@@ -1,13 +1,27 @@
-export const TOKEN_KEY = "@e-uleio-Token";
-
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+export const TOKEN_KEY = "@e-uleio/token";
 
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 
-export const login = token => {
+export const isAuthenticated = () => {
+    const token = getToken();
+
+    if (!token) return false;
+    try {
+        const { exp } = JSON.parse(atob(token.split('.')[1]));
+        if (exp < new Date().getTime() / 1000) {
+            localStorage.removeItem(TOKEN_KEY);
+            return false;
+        }
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+export const login = (token) => {
     localStorage.setItem(TOKEN_KEY, token);
-};
+}
 
 export const logout = () => {
     localStorage.removeItem(TOKEN_KEY);
-};
+}
