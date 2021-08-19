c7115674e2c635d887fb1d11495d7084468d7fb6
ceconcarlsen <ceconcarlsen@gmail.com>
2021-08-19 14:24:39 -0300

    alguns ajustes

--- e-uleio/src/pages/Home/index.js
+++ e-uleio/src/pages/Home/index.js
@@ -4,13 +4,10 @@ import './style.css'
 function Home(){
     return(
         <div className="home-container">
-
             <section className="logo">
                 <img src="/assets/books-icon.png" alt="Logo"></img>
                 <p>Onde as <strong>melhores histórias</strong> se encontram.</p>
             </section>
-
-            dia 1 1:15:20
         </div>
     )
 }
--- e-uleio/src/routes.js
+++ e-uleio/src/routes.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { isAuthenticated } from './services/auth';
 
 import Cart from './pages/Ecommerce/Cart';
-import Home from './pages/Ecommerce/Home';
+import Home from './pages/Home';
 import Ecommerce from './pages/Ecommerce';
 import Login from './pages/Login';
 import Profile from './pages/Ecommerce/Profile';
